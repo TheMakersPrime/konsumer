c378c75c32ba5440f7e25114df861d00b17b5d2c
ishowrunes <ishworisgod@gmail.com>
2025-07-24 16:08:24 +0545

    Add concrete implementation for emit function

--- lib/src/base/konsumer_view_model.dart
+++ lib/src/base/konsumer_view_model.dart
@@ -4,7 +4,10 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:konsumer/src/base/konsumer_state.dart';
 
 abstract class KonsumerViewModel<S extends KonsumerState> extends AutoDisposeNotifier<S> {
-  S emit(S state);
+  // ignore: use_setters_to_change_properties
+  void emit(S s) {
+    state = s;
+  }
 }
 
 abstract class StickyKonsumerViewModel<S extends KonsumerState> extends Notifier<S> {}
